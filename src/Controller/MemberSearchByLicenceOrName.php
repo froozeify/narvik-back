745ee28d3870ead0aeec495364d66ea3b942bd2e
Benoit VIGNAL <git@benoit-vignal.fr>
2024-02-21 22:20:13 +0100

    Switch for search in POST

--- src/Controller/MemberSearchByLicenceOrName.php
+++ src/Controller/MemberSearchByLicenceOrName.php
@@ -4,11 +4,31 @@ namespace App\Controller;
 
 use App\Repository\MemberRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class MemberSearchByLicenceOrName extends AbstractController {
 
-  public function __invoke(string $id, MemberRepository $memberRepository): ?array {
-    return $memberRepository->findByLicenceOrName($id);
+  public function __invoke(Request $request, MemberRepository $memberRepository, SerializerInterface $serializer): Response {
+    $payload = json_decode($request->getContent(), true);
+    $requiredParams = [
+      'query'
+    ];
+
+    foreach ($requiredParams as $requiredParam) {
+      if (!array_key_exists($requiredParam, $payload)) {
+        throw new HttpException(Response::HTTP_BAD_REQUEST, "Missing required field: '$requiredParam'");}
+    }
+
+    $query = $payload['query'];
+
+    $members = $memberRepository->findByLicenceOrName($query);
+
+    return new Response($serializer->serialize($members, 'json', [
+      'groups' => 'autocomplete'
+    ]));
   }
 
 }

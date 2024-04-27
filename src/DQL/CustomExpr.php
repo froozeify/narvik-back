b02a0dcf6b875d7e6d6663c25a157e753c430a4e
Benoit VIGNAL <git@benoit-vignal.fr>
2024-04-27 19:23:22 +0200

    Make dql query insensitive applied also on the multipleFilter

--- src/DQL/CustomExpr.php
+++ src/DQL/CustomExpr.php
@@ -5,10 +5,25 @@ namespace App\DQL;
 use Doctrine\ORM\Query\Expr;
 
 class CustomExpr {
+
+  /**
+   * Remove accent on char, for exemple é => e
+   * @see https://www.postgresql.org/docs/current/unaccent.html
+   *
+   * @param string $x
+   * @return string
+   */
   public static function unaccent(string $x): string {
     return new Expr\Func('unaccent', [$x]);
   }
 
+  /**
+   * Remove accent and convert it to lowercase.
+   * Useful for search query
+   *
+   * @param string $x
+   * @return string
+   */
   public static function unaccentInsensitive(string $x): string {
     return self::unaccent(new Expr\Func('LOWER', [$x]));
   }

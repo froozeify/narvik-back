8e57fefc3a209d59a861c52974c5359707a8687a
Benoit VIGNAL <git@benoit-vignal.fr>
2024-07-22 10:35:43 +0200

    Update fixture

--- src/Factory/ActivityFactory.php
+++ src/Factory/ActivityFactory.php
@@ -54,6 +54,7 @@ final class ActivityFactory extends PersistentProxyObjectFactory {
     '25M toutes armes',
     '50M rameneurs',
     '50M 22Hunter',
+    'Tir Contrôlé'.
     'Permanence',
     'Travaux',
     'Bureau',

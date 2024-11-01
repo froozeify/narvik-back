dc28676ff54bc3048a94d2677810ebd98d92f640
Benoit VIGNAL <git@benoit-vignal.fr>
2024-11-01 17:45:55 +0100

    Switch to dedicated User object

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,7 @@
 namespace App\Controller;
 
 use App\Enum\GlobalSetting;
-use App\Repository\MemberRepository;
+use App\Repository\UserRepository;
 use App\Service\GlobalSettingService;
 use Gesdinet\JWTRefreshTokenBundle\Generator\RefreshTokenGeneratorInterface;
 use Gesdinet\JWTRefreshTokenBundle\Model\RefreshTokenManagerInterface;
@@ -11,12 +11,12 @@ use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class SecurityController extends AbstractController {
 
   #[Route(path: ['/auth/bdg/{token}'], name: 'auth_bdg', methods: ['POST'])]
-  public function loginBadger(string $token, JWTTokenManagerInterface $JWTTokenManager, RefreshTokenManagerInterface $refreshTokenManager, RefreshTokenGeneratorInterface $refreshTokenGenerator, GlobalSettingService $globalSettingService, MemberRepository $memberRepository): JsonResponse {
+  public function loginBadger(string $token, JWTTokenManagerInterface $JWTTokenManager, RefreshTokenManagerInterface $refreshTokenManager, RefreshTokenGeneratorInterface $refreshTokenGenerator, GlobalSettingService $globalSettingService, UserRepository $userRepository): JsonResponse {
 
     // We get the db token
     $dbToken = $globalSettingService->getRequiredSettingValue(GlobalSetting::BADGER_TOKEN);
@@ -25,7 +25,7 @@ class SecurityController extends AbstractController {
       return new JsonResponse(status: Response::HTTP_NOT_FOUND);
     }
 
-    $badgerUser = $memberRepository->findOneBy([
+    $badgerUser = $userRepository->findOneBy([
       "email" => "badger"
     ]);
 

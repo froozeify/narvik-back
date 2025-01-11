ae5c56507389f449f9a247261248cc827511410e
Benoit VIGNAL <git@benoit-vignal.fr>
2025-01-11 17:57:38 +0100

    Reorder files

--- src/Controller/ClubDependent/MemberPhotosImportFromItac.php
+++ src/Controller/ClubDependent/MemberPhotosImportFromItac.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\ClubDependent\Plugin\Presence;
+namespace App\Controller\ClubDependent;
 
 use App\Controller\Abstract\AbstractClubDependentController;
 use App\Service\ImageService;

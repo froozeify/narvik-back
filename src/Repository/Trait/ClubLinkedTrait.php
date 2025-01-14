797413950dc29aef386a4d4a711baf12900faa98
Benoit VIGNAL <git@benoit-vignal.fr>
2025-01-14 19:09:21 +0100

    Add new club linked repository method

--- src/Repository/Trait/ClubLinkedTrait.php
+++ src/Repository/Trait/ClubLinkedTrait.php
@@ -3,15 +3,34 @@
 namespace App\Repository\Trait;
 
 use App\Entity\Club;
+use App\Entity\Interface\UuidEntityInterface;
 
 trait ClubLinkedTrait {
   public function findAllByClub(Club $club): array {
     $qb = $this->createQueryBuilder('e');
     $query = $qb
-      ->andWhere("e.club = :club")
+      ->andWhere($qb->expr()->eq('e.' . $this->getClassName()::getClubSqlPath(), ':club'))
       ->setParameter('club', $club)
       ->getQuery();
 
     return $query->getResult();
+  }
+
+  public function findOneByClubAndUuid(Club $club, string $uuid): ?UuidEntityInterface {
+    $qb = $this->createQueryBuilder('e');
+    $query = $qb
+      ->andWhere('e.uuid = :uuid')
+      ->andWhere($qb->expr()->eq('e.' . $this->getClassName()::getClubSqlPath(), ':club'))
+      ->setParameter('uuid', $uuid)
+      ->setParameter('club', $club)
+      ->setMaxResults(1)
+      ->getQuery();
+
+    try {
+      return $query->getOneOrNullResult();
+    }
+    catch (\Exception $e) {
+      return null;
+    }
   }
 }

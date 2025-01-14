778d6791340eb6dfb84114fbeb24214a66b5cc17
Benoit VIGNAL <git@benoit-vignal.fr>
2025-01-14 19:09:46 +0100

    InventoryItemHistory migrated

--- src/Entity/ClubDependent/Plugin/Sale/InventoryItemHistory.php
+++ src/Entity/ClubDependent/Plugin/Sale/InventoryItemHistory.php
@@ -5,29 +5,37 @@ namespace App\Entity\ClubDependent\Plugin\Sale;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Link;
 use App\Entity\Abstract\UuidEntity;
+use App\Entity\Club;
 use App\Entity\Interface\TimestampEntityInterface;
 use App\Entity\Trait\TimestampTrait;
+use App\Enum\ClubRole;
 use App\Repository\ClubDependent\Plugin\Sale\InventoryItemHistoryRepository;
+use App\State\InventoryItemHistoryProvider;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: InventoryItemHistoryRepository::class)]
 #[ApiResource(
-  uriTemplate: '/inventory-items/{itemId}/histories.{_format}',
+  uriTemplate: '/clubs/{clubUuid}/inventory-items/{itemUuid}/histories.{_format}',
   operations: [
-    new GetCollection(),
+    new GetCollection(
+      security: "is_granted('".ClubRole::supervisor->value."', request)",
+    ),
   ],
   uriVariables: [
-    'itemId' => new Link(toProperty: 'item', fromClass: InventoryItem::class),
+    'itemUuid' => new Link(toProperty: 'item', fromClass: InventoryItem::class),
+    'clubUuid' => new Link(toClass: Club::class),
   ],
   normalizationContext: [
     'groups' => ['inventory-item-history', 'inventory-item-history-read']
   ],
   order: ['createdAt' => 'DESC'],
+  provider: InventoryItemHistoryProvider::class,
 )]
 #[ApiFilter(OrderFilter::class, properties: ['createdAt' => 'DESC'])]
 class InventoryItemHistory extends UuidEntity implements TimestampEntityInterface {

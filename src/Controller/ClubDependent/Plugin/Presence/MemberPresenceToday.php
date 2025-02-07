752d8098d6e80569b6e7424452ccae9b68393c0d
Benoit VIGNAL <git@benoit-vignal.fr>
2025-02-07 11:13:14 +0100

    Deleting a member should not delete the presences (just make the link null so we keep it for the stats)

--- src/Controller/ClubDependent/Plugin/Presence/MemberPresenceToday.php
+++ src/Controller/ClubDependent/Plugin/Presence/MemberPresenceToday.php
@@ -3,6 +3,7 @@
 namespace App\Controller\ClubDependent\Plugin\Presence;
 
 use App\Controller\Abstract\AbstractClubDependentController;
+use App\Entity\ClubDependent\Plugin\Presence\MemberPresence;
 use App\Repository\ClubDependent\Plugin\Presence\MemberPresenceRepository;
 use App\Service\GlobalSettingService;
 use App\Service\MemberService;
@@ -11,11 +12,16 @@ class MemberPresenceToday extends AbstractClubDependentController {
 
   public function __invoke(MemberPresenceRepository $memberPresenceRepository, MemberService $memberService, GlobalSettingService $globalSettingService): ?array {
 
+    /** @var MemberPresence[] $todayPresentMembers */
     $todayPresentMembers = $memberPresenceRepository->findAllPresentToday($this->getQueryClub());
 
     $controlShootingActivity = $this->getQueryClub()->getSettings()?->getControlShootingActivity();
 
     foreach ($todayPresentMembers as $memberPresence) {
+      if (!$memberPresence->getMember()) {
+        continue;
+      }
+
       $memberService->setCurrentSeason($memberPresence->getMember());
       if ($controlShootingActivity) {
         $memberService->setLastControlShooting($memberPresence->getMember(), $controlShootingActivity);

8565f30d455338d7cac0a6e81c755ca0460b38e5
Benoit VIGNAL <git@benoit-vignal.fr>
2025-02-11 17:16:40 +0100

    Made season name unique

--- src/Entity/Season.php
+++ src/Entity/Season.php
@@ -14,9 +14,11 @@ use App\Repository\SeasonRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: SeasonRepository::class)]
+#[UniqueEntity(fields: ['name'])]
 #[ApiResource(
   operations: [
     new GetCollection(),
